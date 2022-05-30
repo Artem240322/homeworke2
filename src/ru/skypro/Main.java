1311b33ee68284780a11142d68ce481bff896398
ArtemCH240322 <sun2383@mail.ru>
2022-05-30 16:23:47 +0300

    ДЗ задачи 9-10

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,13 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Person person = new Person("Alla", "Pugacheva");
+        changeValue(person);
+        System.out.println(person);
+    }
+
+    private static void changeValue(Person value) {
+        value.setName("Maxsim");
+        value.setSurname("Galkin");
     }
 }
